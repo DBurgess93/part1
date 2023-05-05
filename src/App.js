bc57f220feb54bb23486c0783f9c6df5c3a7f65e
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-05 14:07:42 +0930

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Hello = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
-  );
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Daniel', age: 29 },
+    { name: 'Bon', age: 30 },
+  ]
+  return (
+    <div>
+      <p> {friends[0].name} {friends[0].age} </p>
+      <p> {friends[1].name} {friends[1].age} </p>
+    </div>
+  )
 }
 
 export default App;
