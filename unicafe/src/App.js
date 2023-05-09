f9dfa00666727819b95135bc6758f769bc06f8d2
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-09 15:47:17 +0930

    Add total, average and percentage of positive

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -8,13 +8,16 @@ const Button = (props) => {
   )
 }
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
   return (
     <div>
       <h1>Statistics</h1>
       <p>good {good} </p>
       <p>neutral {neutral} </p>
       <p>bad {bad} </p>
+      <p> total reviews {total} </p>
+      <p>average {average} </p>
+      <p> positive {positive} % </p>
     </div>
   )
 }
@@ -23,16 +26,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const average = (good - bad) / total || 0;
+  const positive = (good / total * 100)
 
   const handleGoodClick = () => {
+    setTotal(total + 1)
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
+    setTotal(total + 1)
     setNeutral(neutral + 1)
   }
 
   const handleBadClick = () => {
+    setTotal(total + 1)
     setBad(bad + 1)
   }
 
@@ -42,7 +51,14 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='good' />
       <Button handleClick={handleNeutralClick} text='neutral' />
       <Button handleClick={handleBadClick} text='bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average.toFixed(12)}
+        positive={positive}
+      />
     </div>
   );
 }
